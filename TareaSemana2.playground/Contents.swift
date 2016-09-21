8e9e70e2e339f34867f62d17cbdac6e74e224129
Rafael Villegas <rafaelvillegas@macbook-pro-de-rafael.local>
2016-09-21 11:50:18 -0500

    Primera version de "Juego de Memoria"

---
+++ TareaSemana2.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var r = 0...100
+
+for r in 0...100{
+    
+if r >= 30 && r <= 40{
+        print("\(r) Viva Swift")
+    }
+
+if r % 5 == 0 {
+    print("\(r) Bîngo!!!")
+}
+if r % 2 == 0 {
+    print("\(r) par!!!")
+}
+else{
+    print("\(r) impar!!!")
+    }
+
+}
